581e7eb4d8aa90c086665080a14a86c202d5757d
Yusuke Shimada <cymady@gmail.com>
2015-06-08 14:04:07 +0100

    Add new pages of Local Office Approver and Local Office Admin ServiceDesk

---
+++ Brother.WebSites.Core/Pages/MPSTwo/LocalOfficeAdminServiceDeskPage.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Brother.Tests.Selenium.Lib.Support.HelperClasses;
+using Brother.WebSites.Core.Pages.Base;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace Brother.WebSites.Core.Pages.MPSTwo
+{
+    public class LocalOfficeAdminServiceDeskPage : BasePage
+    {
+        public static string Url = "/";
+
+        public override string DefaultTitle
+        {
+            get { return string.Empty; }
+        }
+
+        [FindsBy(How = How.Id, Using = "content_1_InputEmailAddress_Input")]
+        private IWebElement EmailAddressElement;
+        [FindsBy(How = How.Id, Using = "content_1_ButtonNext")]
+        private IWebElement SaveButtonElement;
+        [FindsBy(How = How.CssSelector, Using = ".mps-tabs-main a[href='/mps/local-office/service-desk/users]")]
+        private IWebElement ServiceDeskUsersLinkElement;
+        [FindsBy(How = How.CssSelector, Using = ".mps-tabs-main a[href='/mps/local-office/service-desk/settings']")]
+        private IWebElement ServiceDeskSettingsLinkElement;
+
+        private void IsServiceDeskUsersLinkElementAvailable()
+        {
+            if (ServiceDeskUsersLinkElement == null)
+                throw new NullReferenceException("Create Service desk users link");
+        }
+
+        private void IsServiceDeskSettingsElementAvailable()
+        {
+            if (ServiceDeskSettingsLinkElement == null)
+                throw new NullReferenceException("Create Service desk settings link");
+        }
+
+        public void EnterEmailAddress(string emailaddress)
+        {
+            ClearAndType(EmailAddressElement, emailaddress);
+        }
+        
+        public void ClickSave()
+        {
+                SaveButtonElement.Click();
+        }
+
+        public void NavigateToServiceDeskUsersPage()
+        {
+            IsServiceDeskUsersLinkElementAvailable();
+            ServiceDeskUsersLinkElement.Click();
+        }
+
+        public void NavigateToServiceDeskSettingsPage()
+        {
+            IsServiceDeskSettingsElementAvailable();
+            ServiceDeskSettingsLinkElement.Click();
+        }
+    }
+}
---
+++ Brother.WebSites.Core/Pages/MPSTwo/LocalOfficeApproverProposalsPage.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Brother.WebSites.Core.Pages.Base;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace Brother.WebSites.Core.Pages.MPSTwo
+{
+    public class LocalOfficeApproverProposalsPage : BasePage
+    {
+        public static string Url = "/";
+
+        public override string DefaultTitle
+        {
+            get { return string.Empty; }
+        }
+
+        [FindsBy(How = How.CssSelector, Using = ".mps-tabs-main a[href='/mps/local-office/proposals/awaiting-approval']")]
+        private IWebElement AwaitingApprovalLinkElement;
+        [FindsBy(How = How.CssSelector, Using = ".mps-tabs-main a[href='/mps/local-office/proposals/approved']")]
+        private IWebElement ApprovedLinkElement;
+        [FindsBy(How = How.CssSelector, Using = ".mps-tabs-main a[href='/mps/local-office/proposals/declined']")]
+        private IWebElement DeclinedLinkElement;
+
+        public void IsAwaitingApprovalLinkAvailable()
+        {
+            if (AwaitingApprovalLinkElement == null) 
+                throw new Exception("Unable to locate Awaiting Approval Link");
+
+            AssertElementPresent(AwaitingApprovalLinkElement, "Create New Awaiting Approval Link");
+        }
+
+        public void IsApprovedLinkAvailable()
+        {
+            if (ApprovedLinkElement == null)
+                throw new Exception("Unable to locate Approved");
+
+            AssertElementPresent(ApprovedLinkElement, "Create New Approved Link");
+        }
+
+        public void IsDeclinedLinkAvailable()
+        {
+            if (DeclinedLinkElement == null)
+                throw new Exception("Unable to locate Delicind");
+
+            AssertElementPresent(ApprovedLinkElement, "Create New Delicind Link");
+        }
+
+    }
+}
