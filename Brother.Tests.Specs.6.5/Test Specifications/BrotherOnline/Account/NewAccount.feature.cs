a16cf40229ec9da1bb29b0d8d31d97a8319be63a
ant <anthony.howell@brother.co.uk>
2015-07-27 10:53:59 +0100

    Fixed Build

--- Brother.Tests.Specs.6.5/Test Specifications/BrotherOnline/Account/NewAccount.feature.cs
+++ Brother.Tests.Specs.6.5/Test Specifications/BrotherOnline/Account/NewAccount.feature.cs
@@ -1403,15 +1403,9 @@ this.ScenarioSetup(scenarioInfo);
  testRunner.And("I click on Update details", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
 #line 601
  testRunner.Then("I can verify the email change occurred", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
-<<<<<<< Updated upstream
-#line 576
- testRunner.Then("I validate the new Business Email changes via email", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
-#line 577
-=======
 #line 602
- testRunner.And("If I validate the new changes via email", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
+ testRunner.Then("I validate the new Business Email changes via email", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
 #line 603
->>>>>>> Stashed changes
  testRunner.Then("I can validate the update was successful", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "Then ");
 #line 604
  testRunner.And("I can sign out of Brother Online", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
