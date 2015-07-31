e486540f150cec9ab36d6ecfbd553a2d05804765
ant <anthony.howell@brother.co.uk>
2015-07-31 15:58:51 +0100

    Increased timeout on GetInstance

--- Brother.WebSites.Core/Pages/Base/BasePage.Common.cs
+++ Brother.WebSites.Core/Pages/Base/BasePage.Common.cs
@@ -47,7 +47,7 @@ namespace Brother.WebSites.Core.Pages.Base
             new WebDriverWait(driver, timeSpan).Until(d => d.FindElement(By.TagName("body")));
 
             // Small wait for page to complete loading. Poor but required at this time (increase to 6 seconds if issues discovered)
-            WebDriver.Wait(Helper.DurationType.Second, 4);
+            WebDriver.Wait(Helper.DurationType.Second, 6);
 
             // Performs a second InitElements to refresh Page loading.
             PageFactory.InitElements(driver, pageInstance);
@@ -84,7 +84,7 @@ namespace Brother.WebSites.Core.Pages.Base
             }
 
             // Small wait for page to sort itself out
-            WebDriver.Wait(DurationType.Second, 5);
+            WebDriver.Wait(DurationType.Second, 6);
 
             PageFactory.InitElements(driver, pageInstance);
             return pageInstance;
