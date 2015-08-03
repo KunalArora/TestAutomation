741ed540ad15ae56b1944925fc15cb65475e5776
MitraRid <riddhi.mitra@brother.co.uk>
2015-08-03 16:19:24 +0100

    BBAU-532 feature added which require BOL registration or Italy

---
+++ Brother.Tests.Specs.6.5/Test Steps/Smart Supply/BITTestDiscountSingleItem_BBAU_532Steps.cs
@@ -0,0 +1,18 @@
+﻿using Brother.WebSites.Core.Pages.Base;
+using TechTalk.SpecFlow;
+using Brother.Tests.Selenium.Lib.Pages.BrotherMainSite.Smart_Supply;
+
+
+namespace Brother.Tests.Specs.Test_Steps.Smart_Supply
+{
+    [Binding]
+    public class BITTestDiscountSingleItem_BBAU_532Steps : BaseSteps
+    {
+        [When(@"I  click the smart supply basket icon")]
+        public void WhenIClickTheSmartSupplyBasketIcon()
+        {
+            CurrentPage.As<SmartSupplyProductPage>().clickbasketicon();
+        }
+
+    }
+}
