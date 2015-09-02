56ccf2cddd7f80dbc7af725789595903300ae891
gbhamra <gurpreet.bhamra@brother.co.uk>
2015-09-02 09:22:06 +0100

    Added test steps for the product footer nav tests 8.0.

---
+++ Brother.Tests.Specs.80/Test Steps/BrotherMainSite/FooterNavigation/ProductsFooterSteps.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace Brother.Tests.Specs._80.Test_Steps.BrotherMainSite.FooterNavigation
+{
+    [Binding]
+    public class ProductsFooterSteps
+    {
+        [Then(@"I click the printers option under the products page footer")]
+        public void ThenIClickThePrintersOptionUnderTheProductsPageFooter()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I click the scanners option under the products page footer")]
+        public void ThenIClickTheScannersOptionUnderTheProductsPageFooter()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I am navigated to the scanners page")]
+        public void ThenIAmNavigatedToTheScannersPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I click the label printers option under the products page footer")]
+        public void ThenIClickTheLabelPrintersOptionUnderTheProductsPageFooter()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I am navigated to the label printers page")]
+        public void ThenIAmNavigatedToTheLabelPrintersPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I click the fax machines option under the products page footer")]
+        public void ThenIClickTheFaxMachinesOptionUnderTheProductsPageFooter()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I am navigated to the fax machines page")]
+        public void ThenIAmNavigatedToTheFaxMachinesPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I click the supplies and accessories option under the products page footer")]
+        public void ThenIClickTheSuppliesAndAccessoriesOptionUnderTheProductsPageFooter()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I am navigated to the supplies and accessories page")]
+        public void ThenIAmNavigatedToTheSuppliesAndAccessoriesPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
