c42e1161b35c7b1d73a4a68d34a8ede3d9a40a86
Shweta email = shweta.dawar@brother.co.uk <dawarshw@BIELT386.eu.brothergroup.net>
2015-09-02 09:48:56 +0100

    steps file added for websiteinfo and support in mainsite

---
+++ Brother.Tests.Specs.80/Test Steps/BrotherMainSite/FooterNavigation/SupportMenuSteps.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace Brother.Tests.Specs._80.Test_Steps.BrotherMainSite.FooterNavigation
+{
+    [Binding]
+    public class NavigateFooterSupportMenuSteps
+    {
+        [Given(@"I have clicked the creative centre link in the footer section")]
+        public void GivenIHaveClickedTheCreativeCentreLinkInTheFooterSection()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I have clicked the Downloads/Software link in the footer section")]
+        public void GivenIHaveClickedTheDownloadsSoftwareLinkInTheFooterSection()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I have clicked the Find a Service Centre link in the footer section")]
+        public void GivenIHaveClickedTheFindAServiceCentreLinkInTheFooterSection()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I have clicked the Manuals link in the footer section")]
+        public void GivenIHaveClickedTheManualsLinkInTheFooterSection()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I have clicked the Product Registration link in the footer section")]
+        public void GivenIHaveClickedTheProductRegistrationLinkInTheFooterSection()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"I have clicked the Product Recycling link in the footer section")]
+        public void GivenIHaveClickedTheProductRecyclingLinkInTheFooterSection()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I am navigated to the Creative Centre page")]
+        public void ThenIAmNavigatedToTheCreativeCentrePage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I am navigated to the Downloads/Software page")]
+        public void ThenIAmNavigatedToTheDownloadsSoftwarePage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I am navigated to the Find a Service Centre page")]
+        public void ThenIAmNavigatedToTheFindAServiceCentrePage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I am navigated to the Manuals page")]
+        public void ThenIAmNavigatedToTheManualsPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I am navigated to the Product Registration page")]
+        public void ThenIAmNavigatedToTheProductRegistrationPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"I am navigated to the Recycling page")]
+        public void ThenIAmNavigatedToTheRecyclingPage()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
