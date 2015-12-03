d682ce1322369b0e8085fb0d170825f492f21557
gbhamra <gurpreet.bhamra@brother.co.uk>
2015-12-03 14:09:13 +0000

    Added 8.0 feature and tests for published page verification.

---
+++ Brother.Tests.Specs.80/Test Specification/CMS Sitecore/PublishedPages/PublishedPages.feature
@@ -0,0 +1,19 @@
+﻿@UAT @PROD @TEST
+Feature: PublishedPages
+	In order to validate the success of a new build, 
+	previously published pages are verified to ensure 
+	a CMS code change has not had an adverse effect	
+
+@SMOKE
+Scenario Outline: Navigate to published page to verify all page components 
+	# Given I have navigated to the published url "http://main.co.uk.brotherdv2.eu/QA/TestAutomationPleaseLeave"
+	Given That I navigate to "<Site Url>" in order to validate a published page
+	Then I can verify that the page header is displayed
+	Then I can verify that the search icon is displayed
+	# Then I can verify that the top navigation component is displayed
+	# Then I can verify that the accordion compoment is displayed
+
+Scenarios: 
+	
+	 |Site Url													   |
+	 |http://main.co.uk.brotherdv2.eu/QA/TestAutomationPleaseLeave |
---
+++ Brother.WebSites.Core/Pages/BrotherMainSite/PublishedPage.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Brother.Tests.Selenium.Lib.Support.HelperClasses;
+using Brother.WebSites.Core.Pages.Base;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Brother.Tests.Selenium.Lib.Support.HelperClasses;
+using Brother.WebSites.Core.Pages.Base;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+
+namespace Brother.WebSites.Core.Pages.BrotherMainSite
+{
+    public class PublishedPage : BasePage
+    {
+        public static string Url = "/";
+
+        public override string DefaultTitle
+        {
+            get { return string.Empty; }
+        }
+
+        [FindsBy(How = How.CssSelector, Using = "body > header > div > div > a:nth-child(1)")]
+        public IWebElement PageHeader;
+
+        [FindsBy(How = How.CssSelector, Using = "body > header > div > div > a.common-global-header--toggle.active")]
+        public IWebElement SearchIcon;
+
+        public void GetPublishedPage(string url)
+        {
+            TestCheck.AssertIsEqual(HttpStatusCode.OK, GetWebPageResponse(url), "Incorrect Http Status Code returned");
+
+            WebDriver.SetPageLoadTimeout(TimeSpan.FromSeconds(120));
+
+            WebSites.Core.Pages.General.SiteAccess.ValidateSiteUrl(url);
+            WebDriver.SetPageLoadTimeout(WebDriver.DefaultTimeout);
+        }
+
+        private HttpStatusCode GetWebPageResponse(string webSite)
+        {
+            var responseTimer = new System.Diagnostics.Stopwatch();
+            responseTimer.Start();
+            // get response from WebSite
+            var responseCode = Utils.GetPageResponse(webSite, WebRequestMethods.Http.Get);
+            responseTimer.Stop();
+            var responseTime = responseTimer.Elapsed;
+            Helper.MsgOutput(string.Format("Response time from website [{0}] was [{1}ms]", webSite, responseTime.Milliseconds));
+
+            return responseCode;
+        }
+
+        public void IsPageHeaderDisplayed()
+        {
+            WaitForElementToExistByCssSelector("body > header > div > div > a:nth-child(1)");
+              if (PageHeader == null)
+                 {
+                   throw new NullReferenceException("Unable to locate page header");
+                 }
+            AssertElementPresent(PageHeader, "Page Header", 30);            
+        }
+
+        public void IsSearchIconDisplayed()
+        {
+            WaitForElementToExistByCssSelector("body > header > div > div > a:nth-child(1)");
+            if (SearchIcon == null)
+            {
+                throw new NullReferenceException("Unable to locate search icon");
+            }
+            AssertElementPresent(SearchIcon, "Search Icon", 30);
+        }
+
+                    
+
+
+
+    }
+}
