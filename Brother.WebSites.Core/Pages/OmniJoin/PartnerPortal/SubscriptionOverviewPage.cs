f09cc827bed0a9d4762746060a4f87831dc82ade
sskumar444 <shiva.kumar@brother.co.uk>
2016-02-12 15:06:28 +0000

    Updated the button locater

--- Brother.WebSites.Core/Pages/OmniJoin/PartnerPortal/SubscriptionOverviewPage.cs
+++ Brother.WebSites.Core/Pages/OmniJoin/PartnerPortal/SubscriptionOverviewPage.cs
@@ -18,8 +18,8 @@ namespace Brother.WebSites.Core.Pages.OmniJoin.PartnerPortal
         [FindsBy(How = How.LinkText, Using = "Manage Subscription")]
         public IWebElement  ManageSubscriptionLink;
 
-        [FindsBy(How = How.CssSelector, Using = "#content_0_innercontent_2_btnSubmit")]
-        public IWebElement  ManageSubscriptionSubmitButton;
+        [FindsBy(How = How.CssSelector, Using = "#content_0_innercontent_2_btnPreview")]
+        public IWebElement  ManageSubscriptionUpdatePlanButton;
 
         [FindsBy(How = How.CssSelector, Using = "#content_0_innercontent_2_valNumberOfLicencesRequired")]
         public IWebElement NumberofLicensesRequiredErrorMessage;
@@ -41,11 +41,11 @@ namespace Brother.WebSites.Core.Pages.OmniJoin.PartnerPortal
 
        public SubscriptionOverviewPage ClickManageSubscriptionSubmitButton()
        {
-           if (ManageSubscriptionSubmitButton == null)
+           if (ManageSubscriptionUpdatePlanButton == null)
            {
                throw new NullReferenceException("Unable to locate the ManageSubscriptionSubmitButton");
            }
-           ManageSubscriptionSubmitButton.Click();
+           ManageSubscriptionUpdatePlanButton.Click();
            return GetInstance<SubscriptionOverviewPage>(Driver);
        }
 
