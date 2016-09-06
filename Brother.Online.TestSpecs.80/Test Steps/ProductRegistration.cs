a6cd31c6fb8dfb77d6a2081475f536c0eda142f4
Shweta email = shweta.dawar@brother.co.uk <DawarShw@BIELT386.eu.brothergroup.net>
2016-09-06 15:14:47 +0100

    brotheronlineref and first couple scenario added

---
+++ Brother.Online.TestSpecs.80/Test Steps/ProductRegistration.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Brother.Tests.Selenium.Lib.Support.HelperClasses;
+using Brother.WebSites.Core.Pages.Base;
+using Brother.WebSites.Core.Pages.BrotherOnline.AccountManagement;
+using TechTalk.SpecFlow;
+using Brother.Online.TestSpecs._80.Test_Steps;
+using TechTalk.SpecFlow;
+using TechTalk.SpecFlow.Assist;
+
+namespace Brother.Online.TestSpecs._80.Test_Steps
+{
+    [Binding]
+    public class ProductRegistrationSteps : BaseSteps
+    {
+        [Given(@"I navigate to ""(.*)"" in order to validate a Product Registration page when I want to create a new account or existing account with Brother Online")]
+        public void GivenINavigateToInOrderToValidateAProductRegistrationPageWhenIWantToCreateANewAccountOrExistingAccountWithBrotherOnline(string url)
+        {
+            CurrentPage = BasePage.LoadProductRegistrationPage(CurrentDriver, url);
+            CurrentPage.As<ProductRegistrationPage>().GetProductRegistrationPage(url);
+        }
+
+    }
+
+}
