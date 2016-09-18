09376e44aa614b40c5ebc39a3e584b70e1e02a30
Sayo Afolabi <sayo.afolabi@brother.co.uk>
2016-09-18 21:39:58 +0100

    customer email change for Poland

--- Brother.Tests.Specs.6.5/Test Specifications/MPSTwoByCountries/BIEPC112/BPL/Customer/CloudMPSExistingCustomerCanViewPrintCount.feature.cs
+++ Brother.Tests.Specs.6.5/Test Specifications/MPSTwoByCountries/BIEPC112/BPL/Customer/CloudMPSExistingCustomerCanViewPrintCount.feature.cs
@@ -73,7 +73,7 @@ namespace Brother.Tests.Specs.TestSpecifications.MPSTwoByCountries.BIEPC112.BPL.
         
         [NUnit.Framework.TestAttribute()]
         [NUnit.Framework.DescriptionAttribute("MPS Existing Customer For Run purpose")]
-        [NUnit.Framework.TestCaseAttribute("Cloud MPS Local Office Approver", "Poland", "Purchase & Click with Service", "Minimum Volume", "Cloud MPS Dealer", "Cloud", "Web", "lashonda20160322123145@mailinator.com", "3 lata", "Quarterly in Arrears", "Cloud MPS Customer", null)]
+        [NUnit.Framework.TestCaseAttribute("Cloud MPS Local Office Approver", "Poland", "Purchase & Click with Service", "Minimum Volume", "Cloud MPS Dealer", "Cloud", "Web", "Lashonda20160819112538@mailinator.com", "3 lata", "Quarterly in Arrears", "Cloud MPS Customer", null)]
         public virtual void MPSExistingCustomerForRunPurpose(string role, string country, string contractType, string usageType, string role1, string method, string type, string existingCustomer, string length, string billing, string role2, string[] exampleTags)
         {
             TechTalk.SpecFlow.ScenarioInfo scenarioInfo = new TechTalk.SpecFlow.ScenarioInfo("MPS Existing Customer For Run purpose", exampleTags);
