e08ffbd78f1479c29ad83af5b7dde144f4938c51
Sayo Afolabi <sayo.afolabi@brother.co.uk>
2016-11-04 15:45:35 +0000

    Tagging for NUnit purpose

--- Brother.Tests.Specs.6.5/Test Specifications/MPSTwoByCountries/BIEPC112/BBE/Proposal/CloudMPSDealerCanOperateProposalOffers.feature.cs
+++ Brother.Tests.Specs.6.5/Test Specifications/MPSTwoByCountries/BIEPC112/BBE/Proposal/CloudMPSDealerCanOperateProposalOffers.feature.cs
@@ -23,6 +23,7 @@ namespace Brother.Tests.Specs.TestSpecifications.MPSTwoByCountries.BIEPC112.BBE.
     [NUnit.Framework.CategoryAttribute("MPS")]
     [NUnit.Framework.CategoryAttribute("TEST")]
     [NUnit.Framework.CategoryAttribute("UAT")]
+    [NUnit.Framework.CategoryAttribute("BIEPC112")]
     public partial class CloudMPSBelgianDealerCanOperateProposalOffersFeature
     {
         
@@ -39,7 +40,8 @@ namespace Brother.Tests.Specs.TestSpecifications.MPSTwoByCountries.BIEPC112.BBE.
                     " operate existing proposals", ProgrammingLanguage.CSharp, new string[] {
                         "MPS",
                         "TEST",
-                        "UAT"});
+                        "UAT",
+                        "BIEPC112"});
             testRunner.OnFeatureStart(featureInfo);
         }
         
