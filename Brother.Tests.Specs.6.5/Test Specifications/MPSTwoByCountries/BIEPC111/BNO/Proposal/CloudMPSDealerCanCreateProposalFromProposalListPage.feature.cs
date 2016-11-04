3bc5d72f5958fffc40eb03801ed51767c618f677
Sayo Afolabi <sayo.afolabi@brother.co.uk>
2016-11-04 15:26:02 +0000

    Tagging for NUnit purpose

--- Brother.Tests.Specs.6.5/Test Specifications/MPSTwoByCountries/BIEPC111/BNO/Proposal/CloudMPSDealerCanCreateProposalFromProposalListPage.feature.cs
+++ Brother.Tests.Specs.6.5/Test Specifications/MPSTwoByCountries/BIEPC111/BNO/Proposal/CloudMPSDealerCanCreateProposalFromProposalListPage.feature.cs
@@ -23,6 +23,7 @@ namespace Brother.Tests.Specs.TestSpecifications.MPSTwoByCountries.BIEPC111.BNO.
     [NUnit.Framework.CategoryAttribute("MPS")]
     [NUnit.Framework.CategoryAttribute("UAT")]
     [NUnit.Framework.CategoryAttribute("TEST")]
+    [NUnit.Framework.CategoryAttribute("BIEPC111")]
     public partial class CloudMPSNorwegianDealerCanCreateProposalFromProposalListPageFeature
     {
         
@@ -39,7 +40,8 @@ namespace Brother.Tests.Specs.TestSpecifications.MPSTwoByCountries.BIEPC111.BNO.
                     " new porposal button on proposal list page", ProgrammingLanguage.CSharp, new string[] {
                         "MPS",
                         "UAT",
-                        "TEST"});
+                        "TEST",
+                        "BIEPC111"});
             testRunner.OnFeatureStart(featureInfo);
         }
         
