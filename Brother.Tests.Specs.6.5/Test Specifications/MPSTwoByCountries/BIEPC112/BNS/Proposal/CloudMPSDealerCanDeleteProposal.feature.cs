cfeeba29bdf1b3cb0ea5eab32822b3b2857849c3
Sayo Afolabi <sayo.afolabi@brother.co.uk>
2016-11-04 16:11:16 +0000

    Tagging for NUnit purpose

--- Brother.Tests.Specs.6.5/Test Specifications/MPSTwoByCountries/BIEPC112/BNS/Proposal/CloudMPSDealerCanDeleteProposal.feature.cs
+++ Brother.Tests.Specs.6.5/Test Specifications/MPSTwoByCountries/BIEPC112/BNS/Proposal/CloudMPSDealerCanDeleteProposal.feature.cs
@@ -23,6 +23,7 @@ namespace Brother.Tests.Specs.TestSpecifications.MPSTwoByCountries.BIEPC112.BNS.
     [NUnit.Framework.CategoryAttribute("MPS")]
     [NUnit.Framework.CategoryAttribute("TEST")]
     [NUnit.Framework.CategoryAttribute("UAT")]
+    [NUnit.Framework.CategoryAttribute("BIEPC112")]
     public partial class CloudMPSSwedishDealerCanDeleteProposalFeature
     {
         
@@ -39,7 +40,8 @@ namespace Brother.Tests.Specs.TestSpecifications.MPSTwoByCountries.BIEPC112.BNS.
                     "to delete an open proposal", ProgrammingLanguage.CSharp, new string[] {
                         "MPS",
                         "TEST",
-                        "UAT"});
+                        "UAT",
+                        "BIEPC112"});
             testRunner.OnFeatureStart(featureInfo);
         }
         
