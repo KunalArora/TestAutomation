169765d76e82ed283a6e0ef6048049975821e543
Sayo Afolabi <sayo.afolabi@brother.co.uk>
2016-11-04 16:16:05 +0000

    Tagging for NUnit purpose

--- Brother.Tests.Specs.6.5/Test Specifications/MPSTwoByCountries/BIEPC112/BPL/Customer/CloudMPSCustomerCanPerformSomeRunActions.feature.cs
+++ Brother.Tests.Specs.6.5/Test Specifications/MPSTwoByCountries/BIEPC112/BPL/Customer/CloudMPSCustomerCanPerformSomeRunActions.feature.cs
@@ -24,6 +24,7 @@ namespace Brother.Tests.Specs.TestSpecifications.MPSTwoByCountries.BIEPC112.BPL.
     [NUnit.Framework.CategoryAttribute("TEST")]
     [NUnit.Framework.CategoryAttribute("UAT")]
     [NUnit.Framework.CategoryAttribute("MPS")]
+    [NUnit.Framework.CategoryAttribute("BIEPC112")]
     public partial class PolishCustomerCanPerformSomeRunActionsFeature
     {
         
@@ -41,7 +42,8 @@ namespace Brother.Tests.Specs.TestSpecifications.MPSTwoByCountries.BIEPC112.BPL.
                         "ignore",
                         "TEST",
                         "UAT",
-                        "MPS"});
+                        "MPS",
+                        "BIEPC112"});
             testRunner.OnFeatureStart(featureInfo);
         }
         
