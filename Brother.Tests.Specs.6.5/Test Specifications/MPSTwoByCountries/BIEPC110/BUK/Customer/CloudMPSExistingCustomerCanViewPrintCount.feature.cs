1b7813b45f6035ce86b9b3a6041ca1038903b6d4
Sayo Afolabi <sayo.afolabi@brother.co.uk>
2016-11-04 15:12:48 +0000

    Tagging for NUnit purpose

--- Brother.Tests.Specs.6.5/Test Specifications/MPSTwoByCountries/BIEPC110/BUK/Customer/CloudMPSExistingCustomerCanViewPrintCount.feature.cs
+++ Brother.Tests.Specs.6.5/Test Specifications/MPSTwoByCountries/BIEPC110/BUK/Customer/CloudMPSExistingCustomerCanViewPrintCount.feature.cs
@@ -23,6 +23,7 @@ namespace Brother.Tests.Specs.TestSpecifications.MPSTwoByCountries.BIEPC110.BUK.
     [NUnit.Framework.CategoryAttribute("MPS")]
     [NUnit.Framework.CategoryAttribute("UAT")]
     [NUnit.Framework.CategoryAttribute("TEST")]
+    [NUnit.Framework.CategoryAttribute("BIEPC110")]
     public partial class CloudMPSCreateAContractWithExistingUKCustomerFeature
     {
         
@@ -39,7 +40,8 @@ namespace Brother.Tests.Specs.TestSpecifications.MPSTwoByCountries.BIEPC110.BUK.
                     "ate a contract with an existing customer", ProgrammingLanguage.CSharp, new string[] {
                         "MPS",
                         "UAT",
-                        "TEST"});
+                        "TEST",
+                        "BIEPC110"});
             testRunner.OnFeatureStart(featureInfo);
         }
         
