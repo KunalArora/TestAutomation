0ecff14bddd0696408ca606b2b982a951d66b09e
Sayo Afolabi <sayo.afolabi@brother.co.uk>
2016-11-04 15:50:09 +0000

    Tagging for NUnit purpose

--- Brother.Tests.Specs.6.5/Test Specifications/MPSTwoByCountries/BIEPC112/BIR/Proposal/CloudMPSPurchaseAndClickProposalUserJourney.feature.cs
+++ Brother.Tests.Specs.6.5/Test Specifications/MPSTwoByCountries/BIEPC112/BIR/Proposal/CloudMPSPurchaseAndClickProposalUserJourney.feature.cs
@@ -23,6 +23,7 @@ namespace Brother.Tests.Specs.TestSpecifications.MPSTwoByCountries.BIEPC112.BIR.
     [NUnit.Framework.CategoryAttribute("TEST")]
     [NUnit.Framework.CategoryAttribute("UAT")]
     [NUnit.Framework.CategoryAttribute("MPS")]
+    [NUnit.Framework.CategoryAttribute("BIEPC112")]
     public partial class CloudMPSIrishPurchaseAndClickProposalUserJourneyFeature
     {
         
@@ -40,7 +41,8 @@ namespace Brother.Tests.Specs.TestSpecifications.MPSTwoByCountries.BIEPC112.BIR.
                     "proposal", ProgrammingLanguage.CSharp, new string[] {
                         "TEST",
                         "UAT",
-                        "MPS"});
+                        "MPS",
+                        "BIEPC112"});
             testRunner.OnFeatureStart(featureInfo);
         }
         
