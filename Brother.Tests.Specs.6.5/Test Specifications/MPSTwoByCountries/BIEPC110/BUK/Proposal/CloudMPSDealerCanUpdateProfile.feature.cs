9e58f2aefcb016e18b4f6427e82f0ba9442a115e
Sayo Afolabi <sayo.afolabi@brother.co.uk>
2016-11-04 15:15:21 +0000

    Tagging for NUnit purpose

--- Brother.Tests.Specs.6.5/Test Specifications/MPSTwoByCountries/BIEPC110/BUK/Proposal/CloudMPSDealerCanUpdateProfile.feature.cs
+++ Brother.Tests.Specs.6.5/Test Specifications/MPSTwoByCountries/BIEPC110/BUK/Proposal/CloudMPSDealerCanUpdateProfile.feature.cs
@@ -23,6 +23,7 @@ namespace Brother.Tests.Specs.TestSpecifications.MPSTwoByCountries.BIEPC110.BUK.
     [NUnit.Framework.CategoryAttribute("MPS")]
     [NUnit.Framework.CategoryAttribute("TEST")]
     [NUnit.Framework.CategoryAttribute("UAT")]
+    [NUnit.Framework.CategoryAttribute("BIEPC110")]
     public partial class CloudMPSUKDealerCanUpdateProfileFeature
     {
         
@@ -39,7 +40,8 @@ namespace Brother.Tests.Specs.TestSpecifications.MPSTwoByCountries.BIEPC110.BUK.
                     "ile", ProgrammingLanguage.CSharp, new string[] {
                         "MPS",
                         "TEST",
-                        "UAT"});
+                        "UAT",
+                        "BIEPC110"});
             testRunner.OnFeatureStart(featureInfo);
         }
         
