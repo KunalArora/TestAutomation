9770af1e8c1a1d5771ee1a13b61667883afd8ee9
Sayo Afolabi <sayo.afolabi@brother.co.uk>
2016-11-04 15:32:01 +0000

    Tagging for NUnit purpose

--- Brother.Tests.Specs.6.5/Test Specifications/MPSTwoByCountries/BIEPC111/SmokeTest - Cloud_Email/CloudMPSInstallerCanNavigateThroughEmailInstallation.feature.cs
+++ Brother.Tests.Specs.6.5/Test Specifications/MPSTwoByCountries/BIEPC111/SmokeTest - Cloud_Email/CloudMPSInstallerCanNavigateThroughEmailInstallation.feature.cs
@@ -23,6 +23,7 @@ namespace Brother.Tests.Specs.TestSpecifications.MPSTwoByCountries.BIEPC111.Smok
     [NUnit.Framework.CategoryAttribute("MPS")]
     [NUnit.Framework.CategoryAttribute("UAT")]
     [NUnit.Framework.CategoryAttribute("TEST")]
+    [NUnit.Framework.CategoryAttribute("BIEPC111")]
     public partial class CloudMPSInstallerCanNavigateThroughInstallationForSmokeTestFeature
     {
         
@@ -39,7 +40,8 @@ namespace Brother.Tests.Specs.TestSpecifications.MPSTwoByCountries.BIEPC111.Smok
                     "le to complete installation", ProgrammingLanguage.CSharp, new string[] {
                         "MPS",
                         "UAT",
-                        "TEST"});
+                        "TEST",
+                        "BIEPC111"});
             testRunner.OnFeatureStart(featureInfo);
         }
         
