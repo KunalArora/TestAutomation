03a2928268d8d8a2e1060ab66e4bfb10280d4322
Sayo Afolabi <sayo.afolabi@brother.co.uk>
2016-11-23 14:10:13 +0000

    Changes to CH and new CH error message content verification

---
+++ Brother.Tests.Specs.6.5/Test Specifications/MPSTwoByCountries/BIEPC110/BSW/Proposal/CloudMPSErrorMessageShowsCorrectCurrency.feature
@@ -0,0 +1,30 @@
+﻿@MPS @TEST @UAT @BIEPC110
+Feature: CloudMPSErrorMessageShowsCorrectCurrency
+	In order to ensure that error message correctly displayed currency
+	As a dealer
+	I want to see the currency in error match specified currency
+
+
+Scenario Outline: Currency in product screen error message
+	Given I sign into Cloud MPS as a "<Role>" from "<Country>"
+	And I am on MPS New Proposal Page
+	When I fill Proposal Description for "<ContractType>" Contract type
+	And I select "<CreateOption>" button for customer data capture
+	And I Enter "<UsageType>" usage type "<Contract>" contract length and "<Billing>" billing on Term and Type details  
+	And I "<PriceHardware>" Price Hardware radio button
+	And I display "<Printer>" device screen
+	And enter a quantity of "2" for model
+	And I enter some values for the device
+	And I enter incorrect installation cost of "0.00"
+	Then error message displayed contains the "<Currency>" of the specified country
+	#And I type in click price volume of "<ClickVolume>"
+	#Then I click Save Proposal button on Summary screen
+	#And I am directed to Proposals screen of Proposal List page
+	#And the newly created proposal is displayed on the list
+	And I sign out of Cloud MPS
+	
+
+	Scenarios: 
+	| Role             | Country     | ContractType                 | CreateOption           | UsageType     | Contract | Billing       | PriceHardware | Printer    | ClickVolume | Currency |
+	| Cloud MPS Dealer | Switzerland | Purchase & Click mit Service | Skip customer creation | Pay As You Go | 48       | Quartalsweise | Tick          | HL-L5100DN | 750         | CHF      |
+	
--- Brother.Tests.Specs.6.5/Test Specifications/MPSTwoByCountries/BIEPC110/BSW/Proposal/CloudMPSSummaryPageValidationForAllProposalTypes.feature.cs
+++ Brother.Tests.Specs.6.5/Test Specifications/MPSTwoByCountries/BIEPC110/BSW/Proposal/CloudMPSSummaryPageValidationForAllProposalTypes.feature.cs
@@ -96,6 +96,8 @@ this.ScenarioSetup(scenarioInfo);
  testRunner.And("I am on MPS New Proposal Page", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
 #line 11
  testRunner.When(string.Format("I fill Proposal Description for \"{0}\" Contract type", contractType), ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
+#line 12
+ testRunner.And("I choose an existing contact from the list of available contacts", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
 #line 13
  testRunner.And(string.Format("I Enter \"{0}\" usage type \"{1}\" contract length and \"{2}\" billing on Term and Type" +
                         " details", usageType, contract, billing), ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
@@ -174,6 +176,8 @@ this.ScenarioSetup(scenarioInfo);
  testRunner.And("I am on MPS New Proposal Page", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
 #line 49
  testRunner.When(string.Format("I fill Proposal Description for \"{0}\" Contract type", contractType), ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
+#line 50
+ testRunner.And("I choose an existing contact from the list of available contacts", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
 #line 51
  testRunner.And(string.Format("I Enter \"{0}\" usage type \"{1}\" contract length and \"{2}\" billing on Term and Type" +
                         " details", usageType, contract, billing), ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
@@ -243,6 +247,8 @@ this.ScenarioSetup(scenarioInfo);
  testRunner.And("I am on MPS New Proposal Page", ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
 #line 86
  testRunner.When(string.Format("I fill Proposal Description for \"{0}\" Contract type", contractType), ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "When ");
+#line 87
+ testRunner.And(string.Format("I select \"{0}\" button for customer data capture", createOption), ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
 #line 88
  testRunner.And(string.Format("I Enter \"{0}\" usage type \"{1}\" contract length and \"{2}\" billing on Term and Type" +
                         " details", usageType, contract, billing), ((string)(null)), ((TechTalk.SpecFlow.Table)(null)), "And ");
