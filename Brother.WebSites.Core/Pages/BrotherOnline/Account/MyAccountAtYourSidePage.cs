79cca4cab4c021f9f326be73c940568d4253584e
Steve Walters <steve.walters@brother.co.uk>
2017-08-30 10:40:31 +0100

    New files to support 'At your side' login process

---
+++ Brother.WebSites.Core/Pages/BrotherOnline/Account/MyAccountAtYourSidePage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Brother.WebSites.Core.Pages.Base;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace Brother.WebSites.Core.Pages.BrotherOnline.Account
+{
+    public class MyAccountAtYourSidePage : BasePage
+    {
+        [FindsBy(How = How.CssSelector,
+            Using =
+                ".common--conversion-bar--cta a.btn-info"
+            )]
+        public IWebElement AccessMpsDashboardButton;
+
+        public MyAccountAtYourSidePage()
+        {
+            
+        }
+
+        public TPage ClickAccessMpsDashboardButtonToDashboard<TPage>() where TPage : BasePage, new()
+        {
+            ScrollTo(AccessMpsDashboardButton);
+            AccessMpsDashboardButton.Click();
+            return GetInstance<TPage>(Driver);            
+        }
+
+        private void IsAccessMpsDashboardButtonAvailable()
+        {
+            if (AccessMpsDashboardButton == null)
+            {
+                throw new NullReferenceException("Unable to locate Access MPS Dashboard button on page");
+            }
+            AssertElementPresent(AccessMpsDashboardButton, "Access MPS Dashboard button");
+        }
+    }
+}
---
+++ Brother.WebSites.Core/Pages/BrotherOnline/Account/SignInAtYourSidePage.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Brother.WebSites.Core.Pages.Base;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace Brother.WebSites.Core.Pages.BrotherOnline.Account
+{
+    public class SignInAtYourSidePage : BasePage
+    {
+        [FindsBy(How = How.Id,
+            Using =
+                "email"
+            )]
+        public IWebElement EmailAddressTextBox;
+
+        [FindsBy(How = How.Id,
+            Using =
+                "password"
+            )]
+        public IWebElement PasswordTextBox;
+
+        [FindsBy(How = How.Id, Using = "btnSignin")]
+        public IWebElement SignInButton;
+
+        public SignInAtYourSidePage()
+        {
+        }
+
+        public void PopulateEmailAddressTextBox(string emailAddress)
+        {
+            EmailAddressTextBox.Clear();
+            EmailAddressTextBox.SendKeys(emailAddress);
+            EmailAddressTextBox.SendKeys(Keys.Tab);
+        }
+
+        public void PopulatePassword(string password)
+        {
+            PasswordTextBox.SendKeys(password);
+        }
+
+        public void IsSignInButtonAvailable()
+        {
+            if (SignInButton == null)
+            {
+                throw new NullReferenceException("Unable to locate sign in button on page");
+            }
+            AssertElementPresent(SignInButton, "Sign In Button");
+        }
+
+        public MyAccountAtYourSidePage SignInButtonToMyAccount()
+        {
+            ScrollTo(SignInButton);
+            SignInButton.Click();
+            return GetInstance<MyAccountAtYourSidePage>(Driver);
+        }
+    }
+}
