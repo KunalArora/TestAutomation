5a4f37e91ef211412615fba91d65b5574a169428
KunalArora21 <kunal.arora@brother.co.jp>
2018-01-18 13:04:13 +0900

    Add ContractShiftService class

---
+++ Brother.MPS.Tests.Specs/Services/IContractShiftService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brother.Tests.Specs.Services
+{
+    public interface IContractShiftService
+    {
+        void ContractTimeShiftCommand(int contractId, int timeoffset, string timeoffsetunit, bool generateprintcounts, bool generateinvoices, string printvolume);
+    }
+}
