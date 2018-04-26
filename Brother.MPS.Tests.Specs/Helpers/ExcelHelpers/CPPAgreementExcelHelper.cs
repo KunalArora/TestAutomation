29c304a2da832055c8e86462e8c24c0c01cbc571
Aman Chhabra <aman.chhabra@brother.co.jp>
2018-04-26 17:58:27 +0900

    Validation- part 1

--- Brother.MPS.Tests.Specs/Helpers/ExcelHelpers/CPPAgreementExcelHelper.cs
+++ Brother.MPS.Tests.Specs/Helpers/ExcelHelpers/CPPAgreementExcelHelper.cs
@@ -1,23 +1,125 @@
-﻿using Brother.Tests.Common.Logging;
+﻿using Brother.Tests.Common.ContextData;
+using Brother.Tests.Common.Logging;
 using Brother.Tests.Common.RuntimeSettings;
+using Brother.Tests.Selenium.Lib.Support.HelperClasses;
+using NUnit.Framework;
+using OfficeOpenXml;
+using System.IO;
+using System.Linq;
 
 namespace Brother.Tests.Specs.Helpers.ExcelHelpers
 {
     public class CPPAgreementExcelHelper: ExcelBaseHelper, ICPPAgreementExcelHelper
     {
+        // Report Tab sheet properties
+        private const int Report_Country_Col_No = 1;
+        private const int Report_DealershipNumber_Col_No = 2;
+        private const int Report_DealerName_Col_No = 3;
+        private const int Report_AgreementNumber_Col_No = 4;
+        private const int Report_AgreementName_Col_No = 5;
+        private const int Report_LeadCodeReference_Col_No = 6;
+        private const int Report_DealerReference_Col_No = 7;
+        private const int Report_FinanceReference_Col_No = 8;
+        private const int Report_DateCreated_Col_No = 9;
+        private const int Report_Status_Col_No = 10;
+        private const int Report_UsageType_Col_No = 11;
+        private const int Report_AgreementTerm_Col_No = 12;
+        private const int Report_NumberOfDevices_Col_No = 13;
+        private const int Report_NumberOfDevicesInstalled_Col_No = 14;
+        private const int Report_InstallationTotal_Col_No = 15;
+        private const int Report_ServiceTotal_Col_No = 16;
+        private const int Report_ClickTotal_Col_No = 17;
+        private const int Report_StartDate_Col_No = 18;
+        private const int Report_EndDate_Col_No = 19;
+        private const int Report_SalesRep_Col_No = 20;
+
         private ILoggingService LoggingService { get; set; }
+        private IContextData _contextData;
 
         public CPPAgreementExcelHelper(
             IRuntimeSettings runtimeSettings,
-            ILoggingService loggingService
-            ): base(loggingService, runtimeSettings)
+            ILoggingService loggingService,
+            IContextData contextData): base(loggingService, runtimeSettings)
         {
             LoggingService = loggingService;
+            _contextData = contextData;
         }
 
         public void VerifyAgreementDetails(string excelFilePath)
         {
             LoggingService.WriteLogOnMethodEntry(excelFilePath);
+
+            var fileInfo = new FileInfo(excelFilePath);
+
+            Assert.True(fileInfo.Exists, string.Format("Excel sheet = {0} does not exist", excelFilePath));
+
+            using (ExcelPackage pack = new ExcelPackage(fileInfo))
+            {
+                ExcelWorksheet ws = pack.Workbook.Worksheets.First();
+
+                int rowIndex = 2;
+
+                while (!(HandleNullCase(ws.Cells[rowIndex, Report_AgreementNumber_Col_No].Value) == _contextData.AgreementId.ToString()))
+                {
+                    rowIndex++;
+                    if (rowIndex > GetNumberOfRows(excelFilePath))
+                    {
+                        TestCheck.AssertFailTest(
+                            string.Format(
+                            "Information for agreement {0} not present in the Report tab sheet of excel file {1}", _contextData.AgreementId, excelFilePath));
+                    }
+                }
+
+                TestCheck.AssertIsEqual(
+                    _contextData.Country.CountryIso, HandleNullCase(ws.Cells[rowIndex, Report_Country_Col_No].Value), string.Format(
+                    "Country ISO for agreement {0} in excel file {1} could not be verified", _contextData.AgreementId, excelFilePath));
+
+                TestCheck.AssertIsEqual(
+                    _contextData.DealerSAPAccountNumber, HandleNullCase(ws.Cells[rowIndex, Report_DealershipNumber_Col_No].Value), string.Format(
+                    "Dealership (SAP) Number for agreement {0} in excel file {1} could not be verified", _contextData.AgreementId, excelFilePath));
+
+                TestCheck.AssertIsEqual(
+                    _contextData.DealerName, HandleNullCase(ws.Cells[rowIndex, Report_DealerName_Col_No].Value), string.Format(
+                    "Dealer Name for agreement {0} in excel file {1} could not be verified", _contextData.AgreementId, excelFilePath));
+
+                TestCheck.AssertIsEqual(
+                    _contextData.AgreementName, HandleNullCase(ws.Cells[rowIndex, Report_AgreementName_Col_No].Value), string.Format(
+                    "Agreement Name for agreement {0} in excel file {1} could not be verified", _contextData.AgreementId, excelFilePath));
+
+                TestCheck.AssertIsEqual(
+                    _contextData.LeadCodeReference, HandleNullCase(ws.Cells[rowIndex, Report_LeadCodeReference_Col_No].Value), string.Format(
+                    "Lead code reference for agreement {0} in excel file {1} could not be verified", _contextData.AgreementId, excelFilePath));
+
+                TestCheck.AssertIsEqual(
+                    _contextData.DealerReference, HandleNullCase(ws.Cells[rowIndex, Report_DealerReference_Col_No].Value), string.Format(
+                    "Dealer Reference for agreement {0} in excel file {1} could not be verified", _contextData.AgreementId, excelFilePath));
+
+                TestCheck.AssertIsEqual(
+                    _contextData.LeasingFinanceReference, HandleNullCase(ws.Cells[rowIndex, Report_FinanceReference_Col_No].Value), string.Format(
+                    "Leasing/Financial Reference for agreement {0} in excel file {1} could not be verified", _contextData.AgreementId, excelFilePath));
+
+                // Date Created
+                // Status
+
+                TestCheck.AssertIsEqual(
+                    _contextData.UsageType, HandleNullCase(ws.Cells[rowIndex, Report_UsageType_Col_No].Value), string.Format(
+                    "Usage Type for agreement {0} in excel file {1} could not be verified", _contextData.AgreementId, excelFilePath));
+
+                // Agreement Term in months
+                TestCheck.AssertIsEqual(
+                    _contextData.ContractTerm[0]*12, HandleNullCase(ws.Cells[rowIndex, Report_AgreementTerm_Col_No].Value), string.Format(
+                    "Agreement Term for agreement {0} in excel file {1} could not be verified", _contextData.AgreementId, excelFilePath));
+
+                // Number of devices
+                TestCheck.AssertIsEqual(
+                    _contextData.AdditionalDeviceProperties.Count, HandleNullCase(ws.Cells[rowIndex, Report_NumberOfDevices_Col_No].Value), string.Format(
+                    "Number of devices for agreement {0} in excel file {1} could not be verified", _contextData.AgreementId, excelFilePath));
+
+                // Number of devices installed
+                TestCheck.AssertIsEqual(
+                    _contextData.AdditionalDeviceProperties.Count, HandleNullCase(ws.Cells[rowIndex, Report_NumberOfDevicesInstalled_Col_No].Value), string.Format(
+                    "Number of devices installed for agreement {0} in excel file {1} could not be verified", _contextData.AgreementId, excelFilePath));
+            }
         }
     }
 }
