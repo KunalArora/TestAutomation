7e415fe00245d656680b1066368c509d39ecef08
Jun OIKE <xx0890.ohike@brother.co.jp>
2018-05-07 20:11:16 +0900

    stabilizing: refresh page after RunCreateCustomerAndPersonCommand()

--- Brother.MPS.Tests.Specs/Services/IPageService.cs
+++ Brother.MPS.Tests.Specs/Services/IPageService.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Brother.WebSites.Core.Pages;
+﻿using Brother.WebSites.Core.Pages;
 using Brother.WebSites.Core.Pages.Base;
 using Brother.WebSites.Core.Pages.BrotherOnline.Account;
 using OpenQA.Selenium;
-using OpenQA.Selenium.Support.PageObjects;
 
 namespace Brother.Tests.Specs.Services
 {
@@ -16,5 +10,6 @@ namespace Brother.Tests.Specs.Services
         SignInAtYourSidePage LoadAtYourSideSignInPage(IWebDriver driver = null, string server = null);
         TPage GetPageObject<TPage>(int? timeout = null, IWebDriver driver = null) where TPage : BasePage, IPageObject, new();
         TPage LoadUrl<TPage>(string url, int timeout, string validationElementSelector = null, bool addToContextAsCurrentPage = false, IWebDriver driver = null) where TPage : BasePage, IPageObject, new();
+        TPage Refresh<TPage>(TPage pageObject, int timeout) where TPage : BasePage, IPageObject;
     }
 }
