b702b2e8cb31b77dfd987b15171a778fc8790549
Steve Walters <steve.walters@brother.co.uk>
2018-05-10 07:47:13 +0100

    Update to Powershell scripts and addition of new ones for running tests

---
+++ scripts/mps/RunTests - T3 SmokeTest.ps1
@@ -0,0 +1,6 @@
+﻿Import-Module ".\run-tests.psm1"
+
+RunTests -TestAssembly "Brother.Tests.Specs.6.5.dll" -Category "SMOKE,TYPE3" -UseRelativePath $true
+
+"Done. Press any key."
+cmd /c pause | out-null
--- scripts/mps/run-tests.psm1
+++ scripts/mps/run-tests.psm1
@@ -1,23 +1,44 @@
 ﻿function RunTests(){
 
+	# USAGE NOTES
+	# Ensure -TestAssembly path is correct for the machine on which the tests are running. If this script is in the same folder as the test assembly, just specify the assembly filename and -UseRelativePath $true
+	# The -Category parameter specifies one or more categories to run, separated by commas. Required. 
+	# -Env is one of DEV, TEST, UAT or PROD. Default is UAT.
+	# -LoggingLevel is one of DEBUG,INFO,WARNING or FAILURE. Default is INFO.
+	# -ExploreOnly is $true by default here, this allows you to examine the list of tests in the specified categories without actually running them. Set to $false when you want to pull the trigger!
+	# -UseRelativePath is $false by default which requires a full path to be supplied for -TestAssembly
+	# -AdditionalParams is used to pass arbitrary command line parameters to Nunit
+
     param(
         [string]$TestAssembly = $(throw "Test assembly must be specified"),
-        [string]$category = $(throw "Category must be specified"),
+        [string]$Category = $(throw "Category must be specified"),
         [string]$OutputPath,
         [string]$Env = "UAT",
-        [bool]$ExploreOnly
+		[string]$LoggingLevel = "INFO",
+		[bool]$UseRelativePath = $false,
+        [bool]$ExploreOnly,
+		[string]$AdditionalParams
     )
 
     $date = Get-Date -Format "yyyyMMdd-HHmmss"
     $computerName = hostname
     $exploreOption = ""
+	$scriptPath = Get-Location
 
+	if(-Not [string]::IsNullOrEmpty($AdditionalParams)){
+		$AdditionalParams = ';' + $AdditionalParams
+	}
+	
+	if($UseRelativePath){
+		$TestAssembly = $scriptPath.Path + "\$TestAssembly"
+	}
+	
     $explodedCats = $Category.Split(",")
     $catSelect = ""
 
     foreach($cat in $explodedCats){
     if(-Not ($catSelect -eq "")){
-        $catSelect = $catSelect + " || "
+        $catSelect = $catSelect + " & "
     }
         $catSelect = $catSelect + "cat = $cat & cat = MPS"
     }
@@ -36,17 +57,12 @@
     $outputFile = "$outputPath\log-$date.txt"
 
     cd $nunitPath
-    $outputPath
-    $outputFile
-    #Write-Host "ExploreOnly: $ExploreOnly exploreOption: $exploreOption"
-
-    Test-Path $outputPath
-
+	
     if( -Not (Test-Path $OutputPath ) )
     {
         New-Item $OutputPath -ItemType directory
     }
 
-    .\nunit3-console.exe $TestAssembly --where:"$catSelect" --result=$outputFile --params="env=$Env;output_path='$OutputPath\'" $exploreOption
+    .\nunit3-console.exe $TestAssembly --where:"$catSelect" --result=$outputFile --params="env=$Env;output_path='$OutputPath\';logging_level=$LoggingLevel$AdditionalParams" $exploreOption
 
 }
