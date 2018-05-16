e93b6fd066e2306ebab0704512fd903279977393
Jun OIKE <xx0890.ohike@brother.co.jp>
2018-05-16 18:03:50 +0900

    support for logging service output type

--- Brother.MPS.Tests.Specs/Configuration/CommandLineSettings.cs
+++ Brother.MPS.Tests.Specs/Configuration/CommandLineSettings.cs
@@ -8,12 +8,13 @@ namespace Brother.Tests.Specs.Configuration
         public CommandLineSettings()
         {
             OutputPath = TestContext.Parameters.Get("output_path", null);
-            LoggingLevel = TestContext.Parameters.Get("logging_level",null);
+            LoggingLevel = TestContext.Parameters.Get("logging_level", null);
             EnvironmentUnderTest = TestContext.Parameters.Get("env", null);
-            BaseUrl = TestContext.Parameters.Get("base_url",null);
+            BaseUrl = TestContext.Parameters.Get("base_url", null);
             Culture = TestContext.Parameters.Get("culture", null);
             DealerUsername = TestContext.Parameters.Get("dealer_username", null);
             DealerPassword = TestContext.Parameters.Get("dealer_password", null);
+            LoggingStreamType = TestContext.Parameters.Get("logging_stream_type", null);
 
             if (OutputPath != null)
             {
@@ -29,5 +30,6 @@ namespace Brother.Tests.Specs.Configuration
         public string Culture { get; set; }
         public string DealerUsername { get; set; }
         public string DealerPassword { get; set; }
+        public string LoggingStreamType { get; set; }
     }
 }
