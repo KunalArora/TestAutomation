b1db5e09c4c8380c98993bc55a8ef58458be353d
Jun OIKE <xx0890.ohike@brother.co.jp>
2018-06-08 16:13:10 +0900

    fix ContractShiftBeforeSwapDeviceInstallationRequest() set dummy print count for time shift api

--- Brother.MPS.Tests.Specs/StepActions/Agreement/MpsDealerAgreementStepActions.cs
+++ Brother.MPS.Tests.Specs/StepActions/Agreement/MpsDealerAgreementStepActions.cs
@@ -308,6 +308,13 @@ namespace Brother.Tests.Specs.StepActions.Agreement
             }
             catch
             {
+                // note: run ConstractTimeShiftCommand before PrintCount > 0. 
+                // see https://brother-bie.atlassian.net/browse/MPS-5923?focusedCommentId=147451&page=com.atlassian.jira.plugin.system.issuetabpanels:comment-tabpanel#comment-147451
+                LoggingService.WriteLog(LoggingLevel.WARNING, "ContractShiftBeforeSwapDeviceInstallationRequest() dummy count set to all devices for ContractTimeShiftCommand api");
+                foreach (var prop in _contextData.AdditionalDeviceProperties)
+                {
+                    _deviceSimulatorService.SetPrintCounts(prop.BocDeviceId, 1, 0); // dummy count
+                }
                 _runCommandService.RunMeterReadCloudSyncCommand(_contextData.AgreementId, _contextData.Country.CountryIso);
                 _runCommandService.RunStartContractCommand();
 
