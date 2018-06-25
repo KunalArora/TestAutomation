314ab53b6051524b4e7c4637a64ba9cd442b886f
Jun OIKE <xx0890.ohike@brother.co.jp>
2018-06-25 09:47:36 +0900

    resolve issue

---
+++ Brother.MPS.Tests.Specs/Helpers/ExcelHelpers/DealerReportProperties.cs
@@ -0,0 +1,30 @@
+﻿namespace Brother.Tests.Specs.Helpers.ExcelHelpers
+{
+    public class DealerReportProperties
+    {
+        public string Country { get; set; }     // ex. GB
+        public string DealerName { get; set; }  // ex. MPS-BUK-UAT-Dealer1
+        public string DealershipId { get; set; }// ex. 5af7656b-2bb5-4f17-946f-cdba3f5c70a7
+        public string SalesPerson { get; set; }
+        public string DealerContact { get; set; }   // ex. Partner PortalAdmin
+        public string Address { get; set; }         // ex. 1 Dealer Street Manchester
+        public string Postcode { get; set; }        // ex. M341JE
+        public string TelephoneNumber { get; set; } // ex. 01611111111
+        public string EmailAddress { get; set; }    // ex. MPS-BUK-UAT-Dealer1@brother.co.uk
+        public string FirstActivatedDate { get; set; }  // ex. 25/06/2015
+        public string NumberOfMonths { get; set; }  // ex. 36
+        public string NumberOfProposalsTotal { get; set; }  // ex. 208
+        public string NumberOfProposalsCreated { get; set; }// ex. 136
+        public string NumberOfProposalsCreditRequested { get; set; }// ex. 19
+        public string NumberOfProposalsCreditRejected { get; set; } // ex. 15
+        public string NumberOfProposalsSigned { get; set; } // ex. 8
+        public string NumberOfContractsSigned { get; set; } // ex. 30
+        public string NumberOfDevices { get; set; } // ex. 59
+        public string TotalValueInContracts { get; set; }   // ex. 16,332.00
+        public string TotalMarginInContracts { get; set; }  // ex. 11,284.45
+        public string Quantity { get; set; } // ex. 332,260
+        public string Revenue { get; set; } // ex. 20,577.25
+        public string CommissionPaid { get; set; } // ex. 1,355.15
+
+    }
+}
