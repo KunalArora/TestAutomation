0c97415aa676e366396d4d0da2db78191def6f1a
Jun OIKE <xx0890.ohike@brother.co.jp>
2018-06-27 16:26:52 +0900

    remove comment

--- Brother.WebSites.Core/Pages/MPSTwo/LocalOfficeApproverProposalsPage.cs
+++ Brother.WebSites.Core/Pages/MPSTwo/LocalOfficeApproverProposalsPage.cs
@@ -13,7 +13,6 @@ namespace Brother.WebSites.Core.Pages.MPSTwo
     {
         public static string Url = "/";
 
-        //private const string _validationElementSelector = "table.dataTable";
         private const string _validationElementSelector = "div.js-mps-proposal-list-container";
         private const string _url = "/mps/local-office/approval/proposals/awaiting-approval";
 
